6587517a4afc1fd2f0db153c4c13b7f82ceac383
James Scalise <jamescscalise@gmail.com>
2019-08-05 14:55:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,11 @@ def nyc_pigeon_organizer(data)
   newHash = {}
   data.each {|key, value|
     value.each{|key2, value2|
-      
+      value2.each{|name|
+        if newHash[name]
+          newHash[name][key] = key2
+        else
+        end
     }
   }
 end
